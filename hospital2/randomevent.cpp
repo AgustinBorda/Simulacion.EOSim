e053ca0dc8066ef28fd2023a737b92dd8677b82b
NicoFraschetti <nicolasfraschetti@gmail.com>
2020-04-09 15:08:34 -0300

    random events implementation completed

--- hospital2/randomevent.cpp
+++ hospital2/randomevent.cpp
@@ -12,17 +12,25 @@ void RandomEvent::eventRoutine(Entity* who){
     std::cout << "Evento nuevo en " << who->getClock() << ", flag: " << flag << '\n';
     // se castea owner a un HospitalSimple
 	HospitalSimple& h = dynamic_cast<HospitalSimple&>(owner);
-    if (flag % 2 == 0 && h.camas.isAvailable(1)){
-        std::cout << "Por adquirir una cama, cantidad de camas libres " << h.camas.getQuantity() << '\n';
+    if (flag % 2 == 0 && h.camas.isAvailable(1)) {
+        h.ocupacionCamas.log(h.camas.getMax()-h.camas.getQuantity());
         h.camas.acquire(1);
-    std::cout << "cama adquirida, cantidad de camas libres " << h.camas.getQuantity() << '\n';
+        flag++;
     }
-	else if (h.camas.getMax() - h.camas.getQuantity() > 0){
-	    std::cout << "Por devolver una cama, cantidad de camas libres " << h.camas.getQuantity() << '\n';
+	else if (flag % 2 != 0 && h.camas.getQuantity() < h.camas.getMax()){
+        h.ocupacionCamas.log(h.camas.getMax()-h.camas.getQuantity());
 		h.camas.returnBin(1);
-		std::cout << "cama devuelta, cantidad de camas libres " << h.camas.getQuantity() << '\n';
+		if (!h.cola.empty()) {
+            h.ocupacionCamas.log(h.camas.getMax()-h.camas.getQuantity());
+            h.camas.acquire(1);
+            std::cout << "un paciente fue aceptado en una cama " << h.getSimTime() << "\n";
+            h.lCola.log(h.cola.size());
+            Entity* ent = h.cola.pop();
+            h.tEspera.log(h.getSimTime() - ent->getClock());
+            h.schedule(h.estadia.sample(), ent, salidaP);
+        }
+		flag++;
 	}
     h.schedule(20.0, new Entity(), evento);
-    flag++;
     delete who;
 }
